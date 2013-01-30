176b70b008d6c773fc7b3fa4d6a24c42ca5392ff
Gerard Madorell Sala <skab93@gmail.com>
2013-01-30 16:53:41 +0100

    deleted irrelevant files

--- larv/Examples/barebones_project/PositionComponent.py
+++
@@ -1,6 +0,0 @@
-import larv
-
-class PositionComponent(larv.Component):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
--- larv/Examples/barebones_project/RenderComponent.py
+++
@@ -1,7 +0,0 @@
-import larv
-import pygame
-
-class RenderComponent(larv.Component):
-    def __init__(self, sprite):
-        assert isinstance(sprite, pygame.Surface)
-        self.sprite = sprite
